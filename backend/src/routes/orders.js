a67e99f21badb90c41b00c20a89755f2ee26d4e6
Hồng Duy Điền <duydien3504@gmail.com>
2026-04-03 21:21:23 +0700

    feat: intergrate VNPAY payment for order func

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -6,11 +6,15 @@ const { verifyToken, checkRole } = require('../utils/authHandler');
 // User routes
 router.post('/checkout', verifyToken, ordersController.checkout);
 router.get('/', verifyToken, ordersController.getMyOrders); // Lấy danh sách đơn của user
+//Nhận data từ VNPay (để trên cùng tránh bị match nhầm vào /:id)
+router.get('/vnpay-return', ordersController.vnpayReturn);
+
 router.get('/:id', verifyToken, ordersController.getOrderById); // Chi tiết đơn
 router.patch('/:id/cancel', verifyToken, ordersController.cancelOrder);
 
 // Admin/Manager routes
 router.get('/admin/all', verifyToken, checkRole('ADMIN', 'MANAGER'), ordersController.getAllOrders);
 router.patch('/:id/status', verifyToken, checkRole('ADMIN', 'MANAGER'), ordersController.updateOrderStatus);
+
 
 module.exports = router;
